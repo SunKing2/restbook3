a3ad2738e92d812a1b6fc8754e5eb0b5dd2cacfd
SunKing2 <louievbl4t@gmail.com>
2017-10-08 15:11:34 -0400

    removed most fields from customer, rand tests. green

--- src/main/java/com/restfully/shop/services/CustomerResource.java
+++ src/main/java/com/restfully/shop/services/CustomerResource.java
@@ -1,27 +1,11 @@
 package com.restfully.shop.services;
 
 import com.restfully.shop.domain.Customer;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
+import org.w3c.dom.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.xml.parsers.*;
+import java.io.*;
 import java.net.URI;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -46,10 +30,10 @@ public class CustomerResource {
    public Response createCustomer(InputStream is) {
 	  System.out.println("POST /customers");
       Customer customer = readCustomer(is);
-      customer.setId(idCounter.incrementAndGet());
-      customerDB.put(customer.getId(), customer);
-      System.out.println("Created customer " + customer.getId());
-      return Response.created(URI.create("/customers/" + customer.getId())).build();
+      customer.id = idCounter.incrementAndGet();
+      customerDB.put(customer.id, customer);
+      System.out.println("Created customer " + customer.id);
+      return Response.created(URI.create("/customers/" + customer.id)).build();
 
    }
 
@@ -79,12 +63,7 @@ public class CustomerResource {
       Customer current = customerDB.get(id);
       if (current == null) throw new WebApplicationException(Response.Status.NOT_FOUND);
 
-      current.setFirstName(update.getFirstName());
-      current.setLastName(update.getLastName());
-      current.setStreet(update.getStreet());
-      current.setState(update.getState());
-      current.setZip(update.getZip());
-      current.setCountry(update.getCountry());
+      current.lastName = update.lastName;
    }
    
    @DELETE
@@ -96,14 +75,8 @@ public class CustomerResource {
 
    protected void outputCustomer(OutputStream os, Customer cust) throws IOException {
       PrintStream writer = new PrintStream(os);
-      writer.println("<customer id=\"" + cust.getId() + "\">");
-      writer.println("   <first-name>" + cust.getFirstName() + "</first-name>");
-      writer.println("   <last-name>" + cust.getLastName() + "</last-name>");
-      writer.println("   <street>" + cust.getStreet() + "</street>");
-      writer.println("   <city>" + cust.getCity() + "</city>");
-      writer.println("   <state>" + cust.getState() + "</state>");
-      writer.println("   <zip>" + cust.getZip() + "</zip>");
-      writer.println("   <country>" + cust.getCountry() + "</country>");
+      writer.println("<customer id=\"" + cust.id + "\">");
+      writer.println("   <last-name>" + cust.lastName + "</last-name>");
       writer.println("</customer>");
    }
 
@@ -116,30 +89,12 @@ public class CustomerResource {
          System.out.println("root=" + root);
          Customer cust = new Customer();
          if (root.getAttribute("id") != null && !root.getAttribute("id").trim().equals(""))
-            cust.setId(Integer.valueOf(root.getAttribute("id")));
+            cust.id = Integer.valueOf(root.getAttribute("id"));
          NodeList nodes = root.getChildNodes();
          for (int i = 0; i < nodes.getLength(); i++) {
             Element element = (Element) nodes.item(i);
-            if (element.getTagName().equals("first-name")) {
-               cust.setFirstName(element.getTextContent());
-            }
-            else if (element.getTagName().equals("last-name")) {
-               cust.setLastName(element.getTextContent());
-            }
-            else if (element.getTagName().equals("street")) {
-               cust.setStreet(element.getTextContent());
-            }
-            else if (element.getTagName().equals("city")) {
-               cust.setCity(element.getTextContent());
-            }
-            else if (element.getTagName().equals("state")) {
-               cust.setState(element.getTextContent());
-            }
-            else if (element.getTagName().equals("zip")) {
-               cust.setZip(element.getTextContent());
-            }
-            else if (element.getTagName().equals("country")) {
-               cust.setCountry(element.getTextContent());
+            if (element.getTagName().equals("last-name")) {
+               cust.lastName = element.getTextContent();
             }
          }
          return cust;
